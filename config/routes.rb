bcca01fdba93a922c3daaf899c76fd3d126cb805
bbennett7 <bryn.bennett@gmail.com>
2019-04-30 15:28:18 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'students/search', to: 'students#search_page'
   resources :students
   resources :classrooms
 end
